326180e2053716abe0fa1fdd46c8776b83100b86
Santiagopt97 <santiagopineda99@gmail.com>
2024-10-24 10:40:58 -0500

    Feat: migration and seeder of Status done

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Status;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $statuses = [
+            ['name' => 'active', 'description' => 'currently active'],
+            ['name' => 'cancelled', 'description' => 'currently cancelled'],
+        ];
+
+        foreach ($statuses as $state) {
+            Status::create($state);
+        }
+    }
+}
