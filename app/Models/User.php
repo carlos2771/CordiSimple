d459547aa8d23610fff2397695a9c4c6bd28978b
betancito <jeronimobeta90@gmail.com>
2024-10-25 11:49:35 -0500

    feat: added timestamps fields and set protected values for table and primary key

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
     /**
      * The attributes that are mass assignable.
      *
