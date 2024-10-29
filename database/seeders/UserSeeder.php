09e3bd72442286a4b3fd57e20aa486bc68d869d6
betancito <jeronimobeta90@gmail.com>
2024-10-29 06:28:07 -0500

    fix: add a }

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -62,4 +61,6 @@ class UserSeeder extends Seeder
             User::create($user);
         }
     }
+}
+
 }
